ad9f1a74dd52d65cde137aa4de776bb8438821b0
Willian <willianrocha142@gmail.com>
2026-09-25 15:14:51 -0400

    codigo analisado e consertado, funcionando como foi especificado pelo proessor

--- bin/canal.dart
+++ bin/canal.dart
@@ -1,25 +1,26 @@
 import 'mensagem.dart';
 import 'usuario.dart';
 
-class canal {
-    final string nome;
-    final list <usuario> _membros = [];
-    final list<mensagem> _mensagens = [];
+class Canal {
+  final String nome;
+  final List<Usuario> _membros = [];
+  final List<Mensagem> _mensagens = [];
 
-    canal(this.nome);
+  Canal(this.nome);
 
-    list<usuario> get membros => list.unmodifiable(_membros);
-    list <mensagem> get mensagens => list.unmodifiable(_mensagens);
+  List<Usuario> get membros => List.unmodifiable(_membros);
+  List<Mensagem> get mensagens => List.unmodifiable(_mensagens);
 
-    void adicionarMembro(Usuario usuario) {
-        if (!membros.contains(usuario)) {
-            _membros.add(usuario);
-        }
+  void adicionarMembro(Usuario usuario) {
+    if (!membros.contains(usuario)) {
+      _membros.add(usuario);
     }
-    void adicionarMensagem(Mensagem mensagem) {
-        if (!_membros.contains(mensagem.autor)){
-            adicionarMembro(mensagem.autor);
-        }
-        _mensagens.add(mensagem);
+  }
+
+  void adicionarMensagem(Mensagem mensagem) {
+    if (!_membros.contains(mensagem.autor)) {
+      adicionarMembro(mensagem.autor);
     }
+    _mensagens.add(mensagem);
+  }
 }
